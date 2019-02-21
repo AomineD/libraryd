5f51f04cabb4847b6aa7c480be646b039df3abc4
Server <shaiyafusion30@gmail.com>
2019-02-21 18:14:51 -0400

    Initial commit

---
+++ app/src/main/java/com/dagf/dialogpackage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dagf.dialogpackage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
