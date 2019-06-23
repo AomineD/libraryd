062796053683f154eac2496efb2b2aadba4e5490
AomineD <shaiyafusion30@gmail.com>
2019-06-22 21:02:55 -0500

    New Player Select to

---
+++ dialoglibrary/src/main/java/com/dagf/dialoglibrary/dialog/PopUpSelect.java
@@ -0,0 +1,207 @@
+package com.dagf.dialoglibrary.dialog;
+
+import android.animation.Animator;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.dagf.dialoglibrary.R;
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdError;
+import com.facebook.ads.InterstitialAd;
+import com.facebook.ads.InterstitialAdListener;
+import com.yarolegovich.discretescrollview.DiscreteScrollView;
+
+import java.util.ArrayList;
+
+/**
+ * @author Diego Garcia
+ */
+public class PopUpSelect extends AlertDialog {
+
+
+    private Context mContext;
+    private InterstitialAd interstitialAd;
+    private Uri urr;
+    private String namee;
+    private View Linss;
+    private View backwh;
+
+    private boolean Already;
+    private RelativeLayout rr;
+
+
+    public static boolean edit;
+    public static long item_id;
+    private boolean ok_bol;
+    private DiscreteScrollView scrollView;
+
+    protected PopUpSelect(@NonNull Context context) {
+        super(context);
+
+    }
+
+    public PopUpSelect(Context context, String url, String nam, InterstitialAd ad) {
+        super(context);
+        this.mContext = context;
+        this.urr = Uri.parse(url);
+        this.namee = nam;
+        this.interstitialAd = ad;
+
+
+
+
+    }
+
+    private ArrayList<Integer> drawablesInt = new ArrayList<>();
+    private ArrayList<String> packages = new ArrayList<>();
+
+    public void setDrawables(ArrayList<Integer> dr){
+        this.drawablesInt = dr;
+    }
+
+    public void setPackages(ArrayList<String> strings){
+        this.packages = strings;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_pop_up_layout);
+
+        //animationView.setScale(1f);
+        rr = findViewById(R.id.kk);
+        backwh = findViewById(R.id.white_backg);
+        Linss = findViewById(R.id.lin_ss);
+        scrollView = findViewById(R.id.reprod);
+
+        playerAdapter adapter = new playerAdapter(mContext, drawablesInt);
+
+        scrollView.setAdapter(adapter);
+
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        getWindow().setBackgroundDrawable(ActivityCompat.getDrawable(getContext(), R.color.tran));
+
+        TextView button_cancel = findViewById(R.id.cancel_button);
+
+        TextView button_save = findViewById(R.id.go_player);
+
+
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        button_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Play();
+            }
+        });
+
+
+    }
+
+    private void Play() {
+        if (!Already) {
+
+
+            backwh.setVisibility(View.VISIBLE);
+            Linss.setVisibility(View.GONE);
+            rr.setVisibility(View.GONE);
+            scrollView.setVisibility(View.GONE);
+
+                    if (interstitialAd != null && interstitialAd.isAdLoaded()) {
+                        interstitialAd.setAdListener(new InterstitialAdListener() {
+                            @Override
+                            public void onInterstitialDisplayed(Ad ad) {
+
+                            }
+
+                            @Override
+                            public void onInterstitialDismissed(Ad ad) {
+                                        OpenWithFLIX(scrollView.getCurrentItem());
+
+
+                                dismiss();
+
+
+                                interstitialAd.loadAd();
+                            }
+
+                            @Override
+                            public void onError(Ad ad, AdError adError) {
+
+                            }
+
+                            @Override
+                            public void onAdLoaded(Ad ad) {
+
+                            }
+
+                            @Override
+                            public void onAdClicked(Ad ad) {
+
+                            }
+
+                            @Override
+                            public void onLoggingImpression(Ad ad) {
+
+                            }
+                        });
+
+
+                        interstitialAd.show();
+                    } else {
+                        OpenWithFLIX(scrollView.getCurrentItem());
+                        dismiss();
+                    }
+
+
+                }
+            //  Log.e("MAIN", "Play: CONCRETE");
+            Already = true;
+
+
+        }
+
+
+    private void OpenWithFLIX(int pos) {
+        String packageName = packages.get(pos);
+        try {
+            Intent mx = new Intent(Intent.ACTION_VIEW);
+            mx.setPackage(packageName);
+            mx.setDataAndType(urr, "video/*");
+            mx.putExtra("title", namee);
+            // mx.putExtra("from_start", false);
+
+            mContext.startActivity(mx);
+
+        } catch (Exception e) {
+            Log.e("MAIN", "OpenWithMX: " + e.getMessage());
+
+            try {
+                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
+            } catch (android.content.ActivityNotFoundException ee) {
+                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + packageName)));
+            }
+
+        }
+    }
+
+// ==================================================================== Editar lista user =============================================================== //
+}
---
+++ dialoglibrary/src/main/java/com/dagf/dialoglibrary/dialog/playerAdapter.java
@@ -0,0 +1,54 @@
+package com.dagf.dialoglibrary.dialog;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.dagf.dialoglibrary.R;
+
+import java.util.ArrayList;
+
+
+public class playerAdapter extends RecyclerView.Adapter<playerAdapter.HolderPlayer> {
+
+    private Context mContext;
+    private ArrayList<Integer> intDrawables = new ArrayList<>();
+
+    public playerAdapter(Context mmm,@NonNull ArrayList<Integer> drawables){
+        this.mContext = mmm;
+        this.intDrawables = drawables;
+    }
+
+    @NonNull
+    @Override
+    public HolderPlayer onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.player_item, parent, false);
+        return new HolderPlayer(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HolderPlayer holder, int position) {
+        if(position == 0){
+            holder.icon.setImageDrawable(mContext.getResources().getDrawable(intDrawables.get(position))); // TODO: CAMBIAR A ICONO DESEADO
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return intDrawables.size();
+    }
+
+    class HolderPlayer extends RecyclerView.ViewHolder{
+
+        private ImageView icon;
+        public HolderPlayer(View itemView) {
+            super(itemView);
+            icon = itemView.findViewById(R.id.icon_item);
+        }
+    }
+}
