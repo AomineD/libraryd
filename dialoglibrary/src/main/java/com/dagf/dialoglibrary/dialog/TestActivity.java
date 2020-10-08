3ec9fd362888bb3b0ab429639ab533f01f86329e
AomineD <shaiyafusion30@gmail.com>
2020-10-07 19:13:14 -0500

    cambio en dialog antibaneo 8 2020

---
+++ dialoglibrary/src/main/java/com/dagf/dialoglibrary/dialog/TestActivity.java
@@ -0,0 +1,16 @@
+package com.dagf.dialoglibrary.dialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dagf.dialoglibrary.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
